6c690cd47422fe21fc16899035dbb172e8d8d957
Jonas Winje <jonas.winje@gmail.com>
2019-03-22 09:06:51 +0100

    mlep

--- script.js
+++ script.js
@@ -27,13 +27,15 @@ function random(minclusive, maxclusive) {
   return Math.floor(Math.random() * maxclusive) + minclusive;
 }
 
+const colors = ["#000000", "#FF0000", "#00FF00", "#0000FF"];
+
 function update() {
   clear();
-  drawSquare(vec(random(0, gameSize.x), random(0, gameSize.y)));
-  drawSquare(vec(random(0, gameSize.x), random(0, gameSize.y)));
-  drawSquare(vec(random(0, gameSize.x), random(0, gameSize.y)));
-  drawSquare(vec(random(0, gameSize.x), random(0, gameSize.y)));
-  setTimeout(update, 500);
+  colors.forEach(color => {
+    canvasContext.fillStyle = color;
+    drawSquare(vec(random(0, gameSize.x), random(0, gameSize.y)));
+  });
+  setTimeout(update, 1000);
 }
 
 update();
